d1fe839184c0e89cf3d64f9a10d8e8d752934496
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-31 16:38:13 +0000

    suppression depuis l' API

--- src/app/post/index/index.component.ts
+++ src/app/post/index/index.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { PostService } from '../post.service';
 import { Post } from '../post';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
   
   posts: Post[] = [];
-      
+  
+  form!: FormGroup;
   /*------------------------------------------
   --------------------------------------------
   Created constructor
   --------------------------------------------
   --------------------------------------------*/
-  constructor(public postService: PostService) { }
+  constructor(public postService: PostService) {  }
       
   ngOnInit(): void {
     this.postService.getAll().subscribe((data: Post[])=>{
@@ -36,4 +39,9 @@ export class IndexComponent {
     })
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form submitted:', this.form.value);
+    }
+  }
 }
